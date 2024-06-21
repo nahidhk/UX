02c93f045d0f8d546fa045675b6bd34a2b19afa0
Nahid HK <nahidappltd@gmail.com>
2024-06-21 16:19:50 +0600

    update

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+
+      colors: {
+        'bg-color': '#091420',
+        'bg-dark-color': '#0c1a28',
+        'primary-color': '#4fe98c'
+
+      },
+      width: {
+        'custom-imge-width': '200px'
+      },
+    },
+  },
+  plugins: [],
+}
+
